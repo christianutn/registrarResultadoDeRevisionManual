152cc38ae61e1b95ee907e69ca56c71badb22b1f
Ferreyra Martin <404034@sistemas.frc.utn.edu.ar>
2025-05-22 17:42:53 -0300

    Empezando

---
+++ modelo/Evento_Sismico.py
@@ -0,0 +1,9 @@
+class Evento_Sismico:
+    def __init__(self, fechaHoraOcurrencia, latitudEpicentro, latitudHipocentro, longitudEpicentro, longitudHipocentro, valorMagnitud):
+        self.fechaHoraOcurrencia =  fechaHoraOcurrencia
+        self.latitudEpicentro = latitudEpicentro
+        self.latitudHipocentro = latitudHipocentro
+        self.longitudEpicentro = longitudEpicentro
+        self.longitudHipocentro =  longitudHipocentro
+        self.valorMagnitud = valorMagnitud
+        
