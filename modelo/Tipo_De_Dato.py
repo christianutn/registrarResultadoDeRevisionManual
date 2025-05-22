e85db61fae8e1d1866a6e8bb28aea663f531e4fe
Francisco Germano <403596@sistemas.frc.utn.edu.ar>
2025-05-22 17:59:30 -0300

    Tipo de dato

---
+++ modelo/Tipo_De_Dato.py
@@ -0,0 +1,5 @@
+class Tipo_De_Dato:
+    def __init__(self, denominacion, nombreUnidadMedida, valorUmbral):
+        self.denominacion =  denominacion
+        self.nombreUnidadMedida = nombreUnidadMedida
+        self.valorUmbral = valorUmbral
