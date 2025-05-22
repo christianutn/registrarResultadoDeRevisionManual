c1753ea69e76f9aeb63a55e9170f144a13a61382
Ferreyra Martin <404034@sistemas.frc.utn.edu.ar>
2025-05-22 18:39:40 -0300

    Cambios finales

--- modelo/Clasificacion_Sismo.py
+++ modelo/Clasificacion_Sismo.py
@@ -1,5 +1,5 @@
-class Clasificacion_Sismo:
+class ClasificacionSismo:
     def __init__(self, km_profundidad_desde, km_profundidad_hasta, nombre):
-        self.km_profundidad_desde =  kmProfundidadDesde
+        self.km_profundidad_desde =  km_profundidad_desde
         self.km_profundidad_hasta = km_profundidad_hasta
         self.nombre = nombre
