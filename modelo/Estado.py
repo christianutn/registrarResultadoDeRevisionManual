a41091483e055fd3148eba11e4b1aebe075d4ced
MariaMak <402291@sistemas.frc.utn.edu.ar>
2025-05-22 18:04:34 -0300

    maru

---
+++ modelo/Estado.py
@@ -0,0 +1,8 @@
+class Estado:
+    def __init__(self, ambito, nombreEstado, esBloqueado, esReachazado):
+        self.ambito = ambito
+        self.nombreEstado = nombreEstado
+        self.esBloqueado = esBloqueado
+        self.esRechazado = esRechazado
+
+        
