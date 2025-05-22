15714fa00728f101e943d1ed2acd90d43240383e
Silberstein Gaspar <400137@sistemas.utn.frc.edu.ar>
2025-05-22 18:31:42 -0300

    correccion

--- modelo/Magnitud_Ritcher.py
+++ modelo/Magnitud_Ritcher.py
@@ -1,6 +1,6 @@
 class Magnitud_Ritcher:
-    def __init__(self, descripcionMagnitud, numero):
-        self.descripcionMagnitud = descripcionMagnitud
+    def __init__(self, descripcion_magnitud, numero):
+        self.descripcion_magnitud = descripcion_magnitud
         self.numero = numero
         
         
