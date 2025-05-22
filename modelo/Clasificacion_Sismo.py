e257a655f947303ec1df703cccc6791badd32783
Silberstein Gaspar <400137@sistemas.utn.frc.edu.ar>
2025-05-22 17:55:58 -0300

    Definicion ClasificacionSismo y EstacionSismologica

---
+++ modelo/Clasificacion_Sismo.py
@@ -0,0 +1,5 @@
+class Clasificacion_Sismo:
+    def __init__(self, kmProfundidadDesde, kmProfundidadHasta, nombre):
+        self.kmProfundidadDesde =  kmProfundidadDesde
+        self.kmProfundidadHasta = kmProfundidadHasta
+        self.nombre = nombre
---
+++ modelo/Estacion_Sismologica.py
@@ -0,0 +1,10 @@
+class Estacion_sismologica:
+    def __init__(self, codigoEstacion, documentoCertificacionAdq, fechaSolicitudCertificacion, latitud, longitud, nombre, nroCertificacionAdquisicion):
+        self.codigoEstacion =  codigoEstacion
+        self.documentoCertificacionAdq = documentoCertificacionAdq
+        self.fechaSolicitudCertificacion = fechaSolicitudCertificacion
+        self.latitud = latitud
+        self.longitud = longitud
+        self.nombre = nombre
+        self.nroCertificacionAdquisicion = nroCertificacionAdquisicion
+        
